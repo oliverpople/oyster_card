180187bf1cd7da1ba2208361a93312fb3269d88d
oliverpople <oliverpople@gmail.com>
2017-10-02 16:44:53 +0100

    Adding oystercard class with intialized balance method.

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,3 +1,12 @@
+require 'oyster_card.rb'
+
 describe OysterCard do
+
+# NameError: uninitialized constant OysterCard
+# ./spec/oyster_card_spec.rb:1:in `<top (required)>'
+describe '#balance'
+  it 'tells customer whether the card has a balance' do
+   expect(subject.balance).to eq 0  
+  end
 
 end
