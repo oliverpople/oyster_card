4b24a51e17d3047eb8e4b9551cc81509e48e7f97
hugosykes <hugo@denmeadfarm.co.uk>
2017-10-02 16:56:05 +0100

    Added top_up method to OysterCard class after writing a failing test

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,9 +1,13 @@
 class OysterCard
 
-attr_reader :balance
+  attr_reader :balance
 
-def initialize
-  @balance = 0
-end
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    @balance += amount
+  end
 
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -4,9 +4,21 @@ describe OysterCard do
 
 # NameError: uninitialized constant OysterCard
 # ./spec/oyster_card_spec.rb:1:in `<top (required)>'
-describe '#balance'
-  it 'tells customer whether the card has a balance' do
-   expect(subject.balance).to eq 0  
+
+  describe '#balance' do
+    it 'tells customer whether the card has a balance' do
+     expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#top up' do
+
+    it 'should top up the card' do
+      val = rand(100)
+      subject.top_up(val)
+      expect(subject.balance).to eq val
+    end
+
   end
 
 end
